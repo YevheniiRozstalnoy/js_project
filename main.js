1e87779a19e1e5150bbf468404e53b8e4bc94a60
Yevhenii <iledevgeniy@gmail.com>
2019-09-25 11:59:35 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+// 'use strict';
+
+a = 5;
+console.log(a);
+
+console.log(4/0);
+console.log('beare'/44);
+console.log(5*5);
+console.log(7*7);
+console.log(8*5);
+
+
+console.log(144+92378)
+var lol
+console.log(lol)
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp'];
+console.log(arr[2]);
+
+//alert("Hello World")
+
+// let answer = confirm("Are you here?")
+
+// console.log(answer);
+
+
+// let answer = prompt("Есть ли вам 18?", "Yes")
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object")
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%3);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
